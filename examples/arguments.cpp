30a3853b72fdda9c4c469c33bd9ee2fd769df1a8
Max Schwarz <max.schwarz@online.de>
2024-11-18 10:06:51 +0100

    clang-format

--- examples/arguments.cpp
+++ examples/arguments.cpp
@@ -3,60 +3,50 @@
 #include <anno.h>
 
 #include <algorithm>
-#include <iostream>
 #include <format>
+#include <iostream>
 #include <sstream>
 
-namespace anns
+template<std::size_t N>
+struct Help
 {
-    template<std::size_t N>
-    struct Help
-    {
-        constexpr Help(const char (&str)[N]) {
-            std::copy_n(str, N, string);
-        }
+  constexpr Help(const char (&str)[N]) { std::copy_n(str, N, string); }
 
-        char string[N];
-    };
+  char string[N];
+};
 
-    struct Short
-    {
-        char key;
-    };
-}
+struct Short
+{
+  char key;
+};
 
 struct Test
 {
-    ANNO(anns::Help{"my help string"}, anns::Short{'h'})
-    bool help = false;
+  ANNO(Help{ "my help string" }, Short{ 'h' })
+  bool help = false;
 
-    ANNO(anns::Help{"input file"}, anns::Short{'i'})
-    ANNO(anns::Short{'c'})
-    ANNO(anns::Short{'d'})
-    std::string_view inputFile;
+  ANNO(Help{ "input file" }, Short{ 'i' })
+  ANNO(Short{ 'c' })
+  ANNO(Short{ 'd' })
+  std::string_view inputFile;
 };
 
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
-    anno::members<Test>().for_each([&](const auto& member){
-        std::string help;
-        if constexpr(auto Help = member.annotations(anno::type<anns::Help>()))
-        {
-            help = Help.get().string;
-        }
+  anno::members<Test>().for_each([&](const auto& member) {
+    std::string help;
+    if constexpr (auto helpList = member.annotations(anno::type<Help>())) {
+      help = helpList.get().string;
+    }
 
-        std::stringstream names;
-        names << "--" << member.name();
+    std::stringstream names;
+    names << "--" << member.name();
 
-        if constexpr(auto Shorts = member.annotations(anno::type<anns::Short>()))
-        {
-            Shorts.for_each([&](const auto& ann){
-                names << ", -" << ann.key;
-            });
-        }
+    if constexpr (auto shortList = member.annotations(anno::type<Short>())) {
+      shortList.for_each([&](const auto& ann) { names << ", -" << ann.key; });
+    }
 
-        std::cout << std::format(" {:<30} {}\n", names.str(), help);
-    });
-
-    Test test;
+    std::cout << std::format(" {:<30} {}\n", names.str(), help);
+  });
 }
