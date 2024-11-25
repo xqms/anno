021e6d8333e078eca7f4110de9c0f71213043d5e
Max Schwarz <max.schwarz@online.de>
2024-11-25 23:04:05 +0100

    doc/build.sh: create output dir if needed

--- doc/build.sh
+++ doc/build.sh
@@ -8,4 +8,5 @@ if [[ ! -e "$DIR/m.css" ]]; then
   git clone https://github.com/mosra/m.css.git "$DIR/m.css"
 fi
 
+mkdir -p "$DIR/../build/doc"
 "$DIR/m.css/documentation/doxygen.py" "$DIR/Doxyfile-mcss"
